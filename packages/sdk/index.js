039e97d1c5d0e6dd9f9a2a6e27367ffa1e171d2c
Seb Ringrose <sebringrose@gmail.com>
2026-09-10 10:27:15 +0100

    fix: dynamically load correct napi binary based on platform

--- packages/sdk/index.js
+++ packages/sdk/index.js
@@ -1,1 +1,11 @@
-module.exports = require('./actualised_sdk.win32-x64-msvc.node');
+const os = require('os');
+const platform = os.platform();
+const arch = os.arch();
+
+if (platform === 'win32' && arch === 'x64') {
+  module.exports = require('./actualised_sdk.win32-x64-msvc.node');
+} else if (platform === 'linux' && arch === 'x64') {
+  module.exports = require('./actualised_sdk.linux-x64-gnu.node');
+} else {
+  throw new Error(`Unsupported platform: ${platform} ${arch}`);
+}
